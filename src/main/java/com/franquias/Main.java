cb3698b11742595bfb64e017af1bd85a4b984e92
Enzo-Gabriell <enzogrledo@gmail.com>
2025-07-16 17:54:41 -0300

    add "Tela"

---
+++ src/main/java/com/franquias/Main.java
@@ -0,0 +1,10 @@
+package com.franquias;
+
+import com.franquias.View.TelaFranquia;
+
+public class Main {
+    public static void main(String[] args) {
+        TelaFranquia tela = new TelaFranquia();
+        tela.desenha();
+    }
+}
---
+++ src/main/java/com/franquias/View/TelaFranquia.java
@@ -0,0 +1,17 @@
+package com.franquias.View;
+
+import javax.swing.JFrame;
+
+public class TelaFranquia {
+    private JFrame tela;
+    private final int WIDTH = 500;
+    private final int HEIGHT = 200;
+    private final int V_GAP = 10;
+    private final int H_GAP = 5;
+
+    public void desenha() {
+
+        tela = new JFrame("Franquia");
+        
+    }
+}
